59fe8a7e056058c8ebf7617cc9143f6bd9620148
Alex Pokhilko <alexfromakb@gmail.com>
2021-11-01 11:26:27 +0300

    Last changes

--- app/src/main/java/ru/apokhilko/composeapparchitecture/ui/composable/main/MainScreen.kt
+++ app/src/main/java/ru/apokhilko/composeapparchitecture/ui/composable/main/MainScreen.kt
@@ -1,16 +1,22 @@
 package ru.apokhilko.composeapparchitecture.ui.composable.main
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.*
+import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
